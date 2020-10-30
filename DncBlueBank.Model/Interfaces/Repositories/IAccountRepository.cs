cecf3f5b91a077b420d10ff2e8e7fe460e036b07
DiegoNanzer <diego_nanzerdiego_nanzer@yahoo.com.br>
2020-10-29 21:44:04 -0300

    Add account interface repository

---
+++ DncBlueBank.Model/Interfaces/Repositories/IAccountRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DncBlueBank.Model.Interfaces.Repositories
+{
+    public interface IAccountRepository
+    {
+        IEnumerable<AccountModel> FindAll();
+    }
+}
